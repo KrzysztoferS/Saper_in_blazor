6b9c8452721b3eccd1eeab6f73f07feaada951c5
Krzysztof Siewko <ksiewko@gmail.com>
2021-10-17 00:36:55 +0200

    Added Timer Component

---
+++ Saper.Blazor/Objects/Timer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Saper.Blazor.Objects
+{
+    public class Timer
+    {
+    }
+}
